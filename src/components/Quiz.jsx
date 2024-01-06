4a864c8da01b018c1e47d7513dee67e4e59c3e62
YERUBANDI SATYA SANTOSH KUMAR <132978331+santoshkumar8704@users.noreply.github.com>
2024-01-06 19:16:39 +0530

    timer optimized

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,35 +1,38 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Question from "../assets/question";
-import COMPLETEDLOGO from "../assets/quiz-complete.png"
+import COMPLETEDLOGO from "../assets/quiz-complete.png";
 import QuestionTimer from "./QuestionTimer";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const CurrentlyActiveQuestion = userAnswers.length;
- 
+
   const isCompleted = CurrentlyActiveQuestion === Question.length;
-  function handleAnswerClick(selectedAnswer) {
+  const handleAnswerClick = useCallback(function handleAnswerClick(
+    selectedAnswer
+  ) {
     setUserAnswers((prev) => {
       return [...prev, selectedAnswer];
     });
-  }
-  if(isCompleted){
-    return(
-        <div id="summary">
-            
-            <img src={COMPLETEDLOGO} alt="completed "/>
-            <h2>QUIZ COMPLETED!</h2>
-        </div>
-    )
-
+  },[]);
+  const handleTimeOut = useCallback(() => {
+    handleAnswerClick(null);
+  }, [handleAnswerClick]);
+  if (isCompleted) {
+    return (
+      <div id="summary">
+        <img src={COMPLETEDLOGO} alt="completed " />
+        <h2>QUIZ COMPLETED!</h2>
+      </div>
+    );
   }
   const shuffledAnswers = [...Question[CurrentlyActiveQuestion].answers];
-  shuffledAnswers.sort(() => Math.random() - 0.5)
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
       <div id="question">
-      <QuestionTimer timeout={10000} onTimeOut={() => {handleAnswerClick(null)}}/>
+        <QuestionTimer key={CurrentlyActiveQuestion} timeout={10000} onTimeOut={handleTimeOut} />
         <h2>{Question[CurrentlyActiveQuestion].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
